38871358f713ac1f3bbf8e5ef59edfbf001b3239
kashyap kitchlu <kitchlukashyap@gmail.com>
2017-10-07 10:03:35 +0000

    after services continue

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,25 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
-public FeedResponse getfreeresponse(String keyword){return null;}
+    public FeedResponse getfeedresponse(String keyword) {
+
+        FeedResponse feedresponseObject = new FeedResponse();
+        try {
+
+            WSRequest feedRequest = WS.url("http://nws.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q",keyword)
+                    .setQueryParameter("output","rss")
+                    .get();
+            Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item = feedResponse.getFirstChild().getFirstChild().getChildNodes().item(9);
+            feedresponseObject.title = item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedresponseObject.pubdate= item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedresponseObject.desciption=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return feedresponseObject;
+
+}
 }
--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -11,5 +11,21 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class NewsAgentService {
-public NewsAgentResponse getNewsAgentResponse(String keyword,UUID sessionid){return null;}
+public NewsAgentResponse getNewsAgentResponse(String keyword,UUID sessionid){
+    NewsAgentResponse newsAgentResponse =new NewsAgentResponse();
+    try {
+        WSRequest queryRequest = WS.url("https://api.api.ai/api.query");
+        CompletionStage<WSResponse> responsePromise = queryRequest
+                .setQueryParameter("v", "20150910")
+                .setQueryParameter("query", "keyword")
+                .setQueryParameter("lang", "en")
+                .setQueryParameter("sessioId",sessionid.toString())
+                .setQueryParameter("timezone", "2017-08-09T03:25:23+0530")
+                .setHeader("Authorization", "Bearer")
+                .get();
+        JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
+        newsAgentResponse.keyword = response.get("result").get("parameters").get("keyword").asText();
+    }
+
+}
 }
