694f87812e960a8f72f2c61185cbfcd6c0c1aef2
kashyap kitchlu <kitchlukashyap@gmail.com>
2017-10-08 07:36:32 +0000

    almost done

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,20 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+$scope.messages =[];
+
+var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    exampleSocket.onmessage  =function  (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+            exampleSocket.send($scope.userMessage);
+            $scope.userMessage = "";
+        };
+        $scope.trust = $sce.trustAsHtml;
 });
