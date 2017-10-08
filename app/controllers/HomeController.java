156471be36972a338d320aa4d6717e0c9f8cbddb
kashyap kitchlu <kitchlukashyap@gmail.com>
2017-10-08 11:30:22 +0000

    deploy final

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,19 +1,10 @@
 package controllers;
 
 import actors.MessageActor;
-import data.LoginForm;
-import play.Configuration;
-import play.data.Form;
-import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.LegacyWebSocket;
 import play.mvc.Result;
-import play.mvc.Results;
 import play.mvc.WebSocket;
-import views.html.login;
-
-import javax.inject.Inject;
-import java.util.Objects;
 
 public class HomeController extends Controller {
     public Result index() {
